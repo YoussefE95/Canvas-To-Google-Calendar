97a30b6c3e8010d9b6502d98622e74878a5df11b
Youssef <youssef95elzein@gmail.com>
2022-10-28 15:09:46 -0700

    separated sensitive info into config json file. a sample is provided for reference

--- web-ext/canvas-api/index.js
+++ web-ext/canvas-api/index.js
@@ -1,14 +1,14 @@
-process.env.CANVAS_API_DOMAIN = '';
-process.env.CANVAS_API_TOKEN = '';
-
 const canvasAPI = require('node-canvas-api');
+const config = require('config.json');
+
+process.env.CANVAS_API_DOMAIN = config.domain;
+process.env.CANVAS_API_TOKEN = config.token;
 
 const getAssignments = async () => {
     try {
         const self = await canvasAPI.getSelf();
         const courses = await canvasAPI.getCoursesByUser(self.id);
         const currentDate = new Date();
-        const assignments = [];
     
         courses.forEach(async (course) => {
             if(!course.access_restricted_by_date) {
@@ -19,22 +19,20 @@ const getAssignments = async () => {
                         const dueDate = new Date(assignment.due_at);
     
                         if(currentDate < dueDate) {
-                            console.log(assignment.name, course.course_code, dueDate);
-                            assignments.push({
+                            const assignmentInfo = {
                                 "name": assignment.name,
                                 "class": course.course_code,
                                 "due": dueDate,
-                            });
+                            };
+                            console.log(assignmentInfo);
                         }
                     }
                 });
             }
         });
-    
-        return assignments;
     } catch(err) {
         console.log(err);
     }
 };
 
-console.log(getAssignments());
+getAssignments();
